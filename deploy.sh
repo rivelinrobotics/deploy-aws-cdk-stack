916f9ea855db3d71346ba7bd58ec009bec7b47cf
Tom Noble <tom@rivelinrobotics.com>
2023-10-04 12:14:38 +0100

    Tidies up shell scripts

--- deploy.sh
+++ deploy.sh
@@ -3,28 +3,31 @@
 STACK_ID=${1}
 ADD_STACK_SUFFIX=${2}
 FULL_STACK_ID=${STACK_ID}
+UNSCOPED_APP=app.py.tmp
+OUTPUT_FILE=.cdk-outputs.json
+CDK_PROJECT=/github/workspace
 
-cd /github/workspace
+cd ${CDK_PROJECT}
 
 if [ ${ADD_STACK_SUFFIX} == "true" ]; then
-    git config --global --add safe.directory /github/workspace
+    git config --global --add safe.directory ${CDK_PROJECT}
     FULL_STACK_ID=${STACK_ID}$(git rev-parse --short HEAD)
-    mv app.py app.py.tmp
-    sed "s/\"${STACK_ID}\"/\"${FULL_STACK_ID}\"/g" app.py.tmp > app.py
+    mv app.py ${UNSCOPED_APP}
+    sed "s/\"${STACK_ID}\"/\"${FULL_STACK_ID}\"/g" ${UNSCOPED_APP} > app.py
 fi
 
 cdk deploy \
     --require-approval never \
-    --outputs-file .cdk-outputs.json \
+    --outputs-file ${OUTPUT_FILE} \
     --exclusively \
     ${FULL_STACK_ID}
 
 chmod a+rw -R cdk.out
-chmod a+rw -R .cdk-outputs.json
+chmod a+rw -R ${OUTPUT_FILE}
 
 if [ ${ADD_STACK_SUFFIX} == "true" ]; then
     rm app.py
-    mv app.py.tmp app.py
+    mv ${UNSCOPED_APP} app.py
 fi
 
-echo "stack-output=$(cat .cdk-outputs.json | jq -c --arg FULL_STACK_ID ${FULL_STACK_ID} '.[$FULL_STACK_ID]')" >> $GITHUB_OUTPUT
+echo "stack-output=$(cat ${OUTPUT_FILE} | jq -c --arg ID ${FULL_STACK_ID} '.[$ID]')" >> $GITHUB_OUTPUT
--- destroy.sh
+++ destroy.sh
@@ -3,26 +3,25 @@
 STACK_ID=${1}
 ADD_STACK_SUFFIX=${2}
 FULL_STACK_ID=${STACK_ID}
+UNSCOPED_APP=app.py.tmp
+CDK_PROJECT=/github/workspace
 
-cd /github/workspace
+cd ${CDK_PROJECT}
 
 if [ ${INPUT_EPHEMERAL} == "true" ]; then
 
     if [ ${ADD_STACK_SUFFIX} == "true" ]; then
-        git config --global --add safe.directory /github/workspace
+        git config --global --add safe.directory ${CDK_PROJECT}
         FULL_STACK_ID=${STACK_ID}$(git rev-parse --short HEAD)
-        mv app.py app.py.tmp
-        sed "s/\"${STACK_ID}\"/\"${FULL_STACK_ID}\"/g" app.py.tmp > app.py
+        mv app.py ${UNSCOPED_APP}
+        sed "s/\"${STACK_ID}\"/\"${FULL_STACK_ID}\"/g" ${UNSCOPED_APP} > app.py
     fi
 
-    cdk destroy \
-        --force \
-        --exclusively \
-        ${FULL_STACK_ID}
+    cdk destroy --force --exclusively ${FULL_STACK_ID}
 
     if [ ${ADD_STACK_SUFFIX} == "true" ]; then
         rm app.py
-        mv app.py.tmp app.py
+        mv ${UNSCOPED_APP} app.py
     fi
 
 else
