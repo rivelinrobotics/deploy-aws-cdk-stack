a8db0a7192b0bd387c48215f6923d637ecfd7aea
Tom Noble <tom@rivelinrobotics.com>
2023-10-10 16:31:47 +0100

    Removes debug prints

--- deploy.sh
+++ deploy.sh
@@ -22,9 +22,6 @@ for PARAMETER in ${PARAMETER_STRING}; do
     CFN_PARAMETERS=$(echo ${CFN_PARAMETERS} | sed "s/${PARAMETER}/--parameters ${PARAMETER}/g")
 done
 
-echo CFN_PARAMETERS=${CFN_PARAMETERS}
-echo AWS_SOFTWARE_SERVICES_ACCOUNT=${AWS_SOFTWARE_SERVICES_ACCOUNT}
-
 cdk deploy \
     --require-approval never \
     --outputs-file ${OUTPUT_FILE} \
