4ea58530e5448170f6eb8cc5e5d3a8c41eda5c46
Tom Noble <tom@rivelinrobotics.com>
2024-06-10 11:11:12 +0100

    Removes parameters. Fixes stack outputs (#5)

--- destroy.sh
+++ destroy.sh
@@ -2,31 +2,15 @@
 
 STACK_ID=${1}
 ADD_STACK_SUFFIX=${2}
-APP_FILE=${4}
-FULL_STACK_ID=${STACK_ID}
-UNSCOPED_APP=${APP_FILE}.tmp
+APP_FILE=${3}
+
 CDK_PROJECT=/github/workspace
 
 cd ${CDK_PROJECT}
 
 if [ ${INPUT_EPHEMERAL} == "true" ]; then
-
-    if [ ${ADD_STACK_SUFFIX} == "true" ]; then
-        git config --global --add safe.directory ${CDK_PROJECT}
-        FULL_STACK_ID=${STACK_ID}$(git rev-parse --short HEAD)
-        mv ${APP_FILE} ${UNSCOPED_APP}
-        sed "s/\"${STACK_ID}\"/\"${FULL_STACK_ID}\"/g" ${UNSCOPED_APP} > ${APP_FILE}
-    fi
-
+    FULL_STACK_ID=$(cdk list | grep ${STACK_ID})
     cdk destroy --force --exclusively ${FULL_STACK_ID}
-
-    if [ ${ADD_STACK_SUFFIX} == "true" ]; then
-        rm ${APP_FILE}
-        mv ${UNSCOPED_APP} ${APP_FILE}
-    fi
-
 else
-
     echo Skipping Stack teardown 
-
 fi
