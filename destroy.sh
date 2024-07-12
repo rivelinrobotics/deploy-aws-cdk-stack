2b58c778175cf60b7d2d9621b3286abfefb3e32a
Tom Noble <tom@rivelinrobotics.com>
2024-07-12 10:31:34 +0100

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-STACK_ID=${1}
+STACK_GLOB=${1}
 ADD_STACK_SUFFIX=${2}
 APP_FILE=${3}
 
@@ -9,8 +9,8 @@ CDK_PROJECT=/github/workspace
 cd ${CDK_PROJECT}
 
 if [ ${INPUT_EPHEMERAL} == "true" ]; then
-    FULL_STACK_ID=$(cdk list | grep ${STACK_ID})
-    cdk destroy --force --exclusively ${FULL_STACK_ID}
+    STACK_IDS=$(cdk list | grep ${STACK_GLOB})
+    cdk destroy --force --exclusively ${STACK_IDS}
 else
     echo Skipping Stack teardown 
 fi
